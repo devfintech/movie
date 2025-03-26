097b4906adf450d6224c3f4fa2573ffd32384e2f
namnvml <namnv@moonlab.asia>
2025-03-26 17:01:55 +0700

    feat: update detail film

--- src/layouts/default-layout.tsx
+++ src/layouts/default-layout.tsx
@@ -1,9 +1,52 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { Outlet } from "react-router-dom"
 
 interface DefaultLayoutProps {}
 
 export const DefaultLayout: FC<DefaultLayoutProps> = () => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "F12") {
+        event.preventDefault()
+      }
+
+      if (event.ctrlKey && event.shiftKey && event.key === "I") {
+        event.preventDefault()
+      }
+
+      if (event.ctrlKey && event.shiftKey && event.key === "J") {
+        event.preventDefault()
+      }
+
+      if (event.ctrlKey && event.key === "U") {
+        event.preventDefault()
+      }
+    }
+
+    const handleContextMenu = (event: MouseEvent) => {
+      event.preventDefault()
+    }
+
+    const detectConsoleOpen = () => {
+      const element = new Image()
+      Object.defineProperty(element, "id", {
+        get: function () {
+          window.location.href = "about:blank"
+        },
+      })
+      console.log(element)
+    }
+
+    document.addEventListener("keydown", handleKeyDown)
+    document.addEventListener("contextmenu", handleContextMenu)
+
+    detectConsoleOpen()
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+      document.removeEventListener("contextmenu", handleContextMenu)
+    }
+  }, [])
   return (
     <div>
       {/* <Header /> */}
--- src/views/example/movie/detail-movie.tsx
+++ src/views/example/movie/detail-movie.tsx
@@ -1,3 +1,4 @@
+import { ArrowLeftOutlined } from "@ant-design/icons"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
@@ -38,8 +39,19 @@ const DetailMovie = () => {
         <div className="absolute h-screen w-screen bg-[linear-gradient(_180deg,_#01040e_8.76%,_rgba(1,_4,_14,_0)_47.49%,_#01040e_100%)]"></div>
         <img src={detailMovie?.poster_url} alt="" className="w-full max-xl:hidden" />
         <img src={detailMovie?.thumb_url} alt="" className="hidden h-full w-full max-xl:flex" />
-        <div className="absolute top-0 cursor-pointer p-4 text-2xl font-medium" onClick={() => navigate(-1)}>
+        <div
+          className="absolute top-0 flex cursor-pointer items-center gap-2 p-4 text-2xl font-medium"
+          onClick={() => navigate("/")}
+        >
+          <ArrowLeftOutlined className="max-w-4" />
           Back
+        </div>
+        <div className="absolute left-1/2 top-1/2 aspect-video -translate-x-1/2 -translate-y-1/2 cursor-pointer overflow-hidden p-4 text-2xl font-medium">
+          <iframe
+            src={embed}
+            frameBorder="0"
+            className="aspect-video h-[calc(100vh_-_15rem)] w-[calc(100vw_-_31rem)]"
+          ></iframe>
         </div>
         <div className="absolute bottom-0 flex flex-col gap-4 p-4">
           <div
@@ -48,7 +60,7 @@ const DetailMovie = () => {
               window.open(embed, "_blank")
             }}
           >
-            {episode}
+            View {episode}
           </div>
           <div className="flex cursor-pointer gap-2">
             {detailMovie?.episodes[0]?.items.map((item: any, index: number) => {
@@ -59,7 +71,7 @@ const DetailMovie = () => {
                       setEmbed(item?.embed)
                       setEpisode(index + 1)
                     }}
-                    className="rounded-lg bg-blue-900 p-1"
+                    className="rounded-lg bg-blue-900 p-2"
                   >
                     {item?.name}
                   </div>
